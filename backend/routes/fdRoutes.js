ea6022d3eb5ab161173427f5231dc4624584a083
Varun Talwai <varuntalwai2020@gmail.com>
2026-06-20 19:14:46 +0530

    Fixed FD delete route

--- backend/routes/fdRoutes.js
+++ backend/routes/fdRoutes.js
@@ -1,25 +1,19 @@
-const express = require('express');
-require('express');
+const express = require("express");
 const router = express.Router();
 
 const {
   createFD,
   getAllFDs,
   deleteFD
-} = require('../controllers/fdController');
-router.delete("/:id", async (req, res) => {
-    try {
-      await db.query(
-        "DELETE FROM fixed_deposits WHERE id = ?",
-        [req.params.id]
-      );
-  
-      res.json({ message: "FD Deleted" });
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  });
-router.post('/', createFD);
-router.get('/', getAllFDs);
+} = require("../controllers/fdController");
+
+// Create New FD
+router.post("/", createFD);
+
+// Get All FDs
+router.get("/", getAllFDs);
+
+// Delete FD by ID
 router.delete("/:id", deleteFD);
+
 module.exports = router;
